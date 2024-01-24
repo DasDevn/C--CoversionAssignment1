bb7a81c3b9a34540134d7c8a33b513fe098f41d8
DasDevn <w0196059@nscc.ca>
2024-01-24 09:30:23 -0400

    Conversion for < and > working.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,30 @@ using namespace std;
 int main() {
 
     string stringConversion = "#include <iostream>\n int main()\n {\n int x=4;\n if (x < 3) x++;\n cout << x << endl;\n }";
-    cout << stringConversion <<endl;
-    return 0;
+
+
+        for (size_t i = 0; i < stringConversion.length(); ++i) {
+            if (stringConversion[i] == '<') {
+                //< gets replaced with &lt
+                stringConversion.replace(i, 1, "&lt");
+                //add 3 to i to go over what we just replaced
+                i += 3;
+            }
+        }
+
+        for (size_t i = 0; i < stringConversion.length(); ++i) {
+            if (stringConversion[i] == '>') {
+                //< gets replaced with &gt
+                stringConversion.replace(i, 1, "&gt");
+                //add 3 to i to go over what we just replaced
+                i += 3;
+            }
+        }
+
+        std::cout << stringConversion << std::endl;
+
+        return 0;
+
 
 
 }
