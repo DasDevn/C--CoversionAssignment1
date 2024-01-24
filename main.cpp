1ddefefc407a13efd5141204c6134888586f4951
DasDevn <w0196059@nscc.ca>
2024-01-24 09:11:14 -0400

    Trying to push

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
 
     string stringConversion = "#include <iostream>\n int main()\n {\n int x=4;\n if (x < 3) x++;\n cout << x << endl;\n }";
-    cout << "Hello, World!" << endl;
     cout << stringConversion <<endl;
     return 0;
 
