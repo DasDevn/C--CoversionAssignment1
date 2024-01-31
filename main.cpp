ad7f77e1262eb0bb21d61046173174f4340c2826
DasDevn <w0196059@nscc.ca>
2024-01-30 21:02:20 -0400

    input and output working. Now add conversion. Figure out later!

--- main.cpp
+++ main.cpp
@@ -3,79 +3,58 @@
 #include <string>
 using namespace std;
 
-void demoOpenPrint()
-{
-    string line;
-    ifstream myFileIn;
-    //you may use either .fail() or the .is_open() method
-    //of the file stream object
-    myFileIn.open("myFileIn.txt");
-    if(myFileIn.is_open())//or if(myFileIn.fail
-    {
-        cout << "File Open" << endl;
-        while(!myFileIn.eof()){
-            getline(myFileIn,line);
-            cout << line<<endl;
-        }
-        myFileIn.close();
-        cout<<"File closed"<<endl;
-    }else{
-        cout << "Input file failed to open"<< endl;
-    }
-}
-
-void demoOpenWrite(const string& fileName) {
-    ofstream myFileOut;
-
-    myFileOut.open(fileName, ios::app);
-
-    if (!myFileOut.fail()) {
-        myFileOut << "stuff" << " and more stuff" << endl;
-        myFileOut.close();
-        cout << "File Closed" << endl;
-    } else {
-        cout << "Output File failed to open" << endl;
-    }
-}
+void demoOpenCloseStreams ();
 
 int main() {
 
+    string stringConversion = "#include <iostream>\n int main()\n {\n int x=4;\n if (x < 3) x++;\n cout << x << endl;\n }";
+    demoOpenCloseStreams ();
 
-
-    string inputFileLocation;
-
-
-
-    cout << "Please enter the full path of your file, only the full path file name will work\n\nex: `C:\C++\Assignemtn1-Conversion` : \n";
-
-    getline(cin, inputFileLocation);
-
-
-        for (size_t i = 0; i < inputFileLocation.length(); ++i) {
-            if (inputFileLocation[i] == '<') {
+        for (size_t i = 0; i < stringConversion.length(); ++i) {
+            if (stringConversion[i] == '<') {
                 //< gets replaced with &lt
-                inputFileLocation.replace(i, 1, "&lt;");
+                stringConversion.replace(i, 1, "&lt;");
                 //add 3 to i to go over what we just replaced
                 i += 4;
             }
         }
 
-        for (size_t i = 0; i < inputFileLocation.length(); ++i) {
-            if (inputFileLocation[i] == '>') {
+        for (size_t i = 0; i < stringConversion.length(); ++i) {
+            if (stringConversion[i] == '>') {
                 //< gets replaced with &gt
-                inputFileLocation.replace(i, 1, "&gt;");
+                stringConversion.replace(i, 1, "&gt;");
                 //add 3 to i to go over what we just replaced
                 i += 4;
             }
         }
 
-    inputFileLocation.insert(0, "<PRE>\n");
+    stringConversion.insert(0, "<PRE>\n");
 
-    inputFileLocation += "\n</PRE>";
-        std::cout << inputFileLocation << std::endl;
+    stringConversion += "\n</PRE>";
+        std::cout << stringConversion << std::endl;
 
         return 0;
 
 
+
+}
+
+void demoOpenCloseStreams () {
+    cout << "Files Open" << endl;
+    //declare stream objects
+    ifstream inStream;
+    ofstream outStream;
+
+    //connect files to stream object
+    inStream.open("C:/C++/Assignment1-Conversion/src/assignmentOneSourceFile.cpp");
+    outStream.open("output.html");
+
+    string line;
+    while (getline(inStream, line)) {
+        outStream << line << endl;
+    }
+
+    inStream.close();
+    outStream.close();
 
 }
--- src/assignmentOneSourceFile.cpp
+++ src/assignmentOneSourceFile.cpp
@@ -1,16 +1,15 @@
-//
-// Created by dasde on 2024-01-30.
-////
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//    int y = 7;
-//    int x=4;
-//    if (y > 3)
-//    {
-//        x++;
-//    }
-//    cout << x << endl;
-//}
+#include <iostream>
+using namespace std;
+
+int writeover()
+{
+    int y = 7;
+    int x=4;
+    if (y > 3)
+    {
+        x++;
+    }
+    cout << x << endl;
+}
+
+//&gt;
