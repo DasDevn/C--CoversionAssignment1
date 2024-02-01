b355060cec001e5b28548eec1ab610f6d63dee78
DasDevn <w0196059@nscc.ca>
2024-01-31 21:23:13 -0400

    INPUT AND OUTPUT working, tidied

--- main.cpp
+++ main.cpp
@@ -3,58 +3,57 @@
 #include <string>
 using namespace std;
 
-void demoOpenCloseStreams ();
+void demoOpenCloseStreams (string);
 
 int main() {
 
-    string stringConversion = "#include <iostream>\n int main()\n {\n int x=4;\n if (x < 3) x++;\n cout << x << endl;\n }";
-    demoOpenCloseStreams ();
-
-        for (size_t i = 0; i < stringConversion.length(); ++i) {
-            if (stringConversion[i] == '<') {
-                //< gets replaced with &lt
-                stringConversion.replace(i, 1, "&lt;");
-                //add 3 to i to go over what we just replaced
-                i += 4;
-            }
-        }
-
-        for (size_t i = 0; i < stringConversion.length(); ++i) {
-            if (stringConversion[i] == '>') {
-                //< gets replaced with &gt
-                stringConversion.replace(i, 1, "&gt;");
-                //add 3 to i to go over what we just replaced
-                i += 4;
-            }
-        }
-
-    stringConversion.insert(0, "<PRE>\n");
-
-    stringConversion += "\n</PRE>";
-        std::cout << stringConversion << std::endl;
-
-        return 0;
-
+    string stringConversion = "C:\\C++\\Assignment1-Conversion\\cmake-build-debug\\output.cpp";
+    demoOpenCloseStreams (stringConversion);
 
 
 }
-
-void demoOpenCloseStreams () {
+void demoOpenCloseStreams(string inputfile) {
     cout << "Files Open" << endl;
-    //declare stream objects
+
+    // Declare stream objects
     ifstream inStream;
     ofstream outStream;
 
-    //connect files to stream object
-    inStream.open("C:/C++/Assignment1-Conversion/src/assignmentOneSourceFile.cpp");
-    outStream.open("output.html");
+    // Connect files to stream objects
+    inStream.open(inputfile);
+    outStream.open("happy.html");
 
     string line;
+    string stringConversion;  // Assuming you have a string to store the entire content
+
+    // ... Read the entire content into stringConversion ...
+
+    // Replace "<" with "&lt;" and ">" with "&gt;" in each line
     while (getline(inStream, line)) {
-        outStream << line << endl;
+        stringConversion += line + "\n";
     }
+
+    // Replace "<" with "&lt;" and ">" with "&gt;" in each line
+    for (int i = 0; i < stringConversion.length(); ++i) {
+        if (stringConversion[i] == '<') {
+            stringConversion.replace(i, 1, "&lt;");
+            // add 3 to i to go over what we just replaced
+            i += 3;
+        } else if (stringConversion[i] == '>') {
+            stringConversion.replace(i, 1, "&gt;");
+            i += 3;
+        }
+    }
+
+    // Insert "<PRE>\n" at the beginning
+    stringConversion.insert(0, "<PRE>\n");
+
+    // Append "\n</PRE>" at the end
+    stringConversion += "\n</PRE>";
+
+    // Write the modified content to the output file
+    outStream << stringConversion << endl;
 
     inStream.close();
     outStream.close();
-
 }
