6ad4d0bf61064930957208d8491d7fde1a283ebc
DasDevn <w0196059@nscc.ca>
2024-02-02 21:31:19 -0400

    exception 1 done

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <regex>
 #include "../inc/htmlConverter.h"
 using namespace std;
 
-
-
+//cite: https://stackoverflow.com/questions/49877965/c-catchstdexception-e-vs-catch
+//exception #1, will handle all exceptions and then throw error with what the error is
 
 int main() {
-    string inputFile = getFileName();
-    string outputFile = getSaveLocation();
-    demoOpenCloseStreams(inputFile, outputFile);
+    try {
+        string inputFile = getFileName();
+        string outputFile = getSaveLocation();
+        demoOpenCloseStreams(inputFile, outputFile);
+    } catch (const exception& e) {
+        cout << "Error: " << e.what() ;
+        return 1;
+    }
+    return 0;
 }
 
